4d4c4a857764741f5b8da2744a33212dbf656f53
Brittany Moliver <britt.moliver@gmail.com>
2023-04-29 15:50:25 -0400

    made backend

--- backend_challenge/app.js
+++ backend_challenge/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const csurf = require("csurf");
-// const helmet = require("helmet");
+const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const { environment } = require("./config");
 const isProduction = environment === "production";
---
+++ backend_challenge/routes/api/index.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+// const sessionRouter = require("./session.js");
+// const usersRouter = require("./users.js");
+// const notesRouter = require("./notes.js")
+
+// router.use("/session", sessionRouter);
+// router.use("/users", usersRouter);
+// router.use("/notes", notesRouter)
+
+router.post("/test", function (req, res) {
+  res.json({ requestBody: req.body });
+});
+
+module.exports = router;
