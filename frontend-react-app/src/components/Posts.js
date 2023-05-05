1e9280cef32402fe37d45fa32b99cd22eda1a392
Brittany Moliver <britt.moliver@gmail.com>
2023-05-05 04:22:07 -0400

    added star rating

--- frontend-react-app/src/components/Posts.js
+++ frontend-react-app/src/components/Posts.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getPosts, removePost } from "../store/post";
+import { StarRating } from "./Assets";
 
 export default function Posts() {
   const dispatch = useDispatch();
@@ -19,6 +20,7 @@ export default function Posts() {
           <div key={post?.id}>
             <div>{post?.title}</div>
             <div>{post?.rating}</div>
+            <div>{StarRating(post?.rating)}</div>
             <div>{post?.text}</div>
             <div>{post?.Coffee?.name}</div>
             <button onClick={() => dispatch(removePost(post))}>X</button>
