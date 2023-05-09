9e33a3e9025974b2bad6edb1948b5fc83b7b1890
Brittany Moliver <britt.moliver@gmail.com>
2023-05-09 09:52:55 -0400

    gotta fix asc and desc buttons

--- frontend-react-app/src/components/Posts.js
+++ frontend-react-app/src/components/Posts.js
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getPosts, removePost } from "../store/post";
 import { StarRating } from "./Assets";
-import CreatePost from "./CreatePost"
+import CreatePost from "./CreatePost";
 
 export default function Posts() {
   const dispatch = useDispatch();
+
   const posts = useSelector((state) => state.postReducer);
-  const postsArr = Object.values(posts);
+  const postsArr = Object.values(posts)
+  const [sortPosts, setSortPosts] = useState('asc')
 
   useEffect(() => {
     dispatch(getPosts());
@@ -18,13 +20,18 @@ export default function Posts() {
       <div>
         <h2>Posts</h2>
         <CreatePost />
-        <button>Asc or Desc</button>
+        <select value={sortPosts} onChange={(e) => setSortPosts(e.target.value)}>
+          <option value={'asc'}>asc</option>
+          <option value={'desc'}>desc</option>
+        </select>
       </div>
       <div>
         {postsArr?.map((post) => (
           <div key={post?.id}>
             <div>{post?.title}</div>
-            <div>{StarRating(post?.rating)} {post?.rating}</div>
+            <div>
+              {StarRating(post?.rating)} {post?.rating}
+            </div>
             <div>{post?.text}</div>
             <div>{post?.Coffee?.name}</div>
             <button onClick={() => dispatch(removePost(post))}>X</button>
